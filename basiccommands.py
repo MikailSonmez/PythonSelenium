eb2ba1c94dd105313c8a07cb70178be91e54b4bd
Mikail Sonmez <mikail4458@hotmail.com>
2022-08-17 18:13:51 +0300

    v2

--- basiccommands.py
+++ basiccommands.py
@@ -1,9 +1,19 @@
+import time
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
 driver.get("https://demo.automationtesting.in/Windows.html")
 
 print(driver.title)  # returns the title of the page
-driver.current_url  # return URL
+print(driver.current_url)  # return URL
+
+driver.find_element(By.XPATH, "//*[@id='Tabbed']/a/button").click()
+
+time.sleep(5)
+
+# driver.close() # crrently focussed browser
+
+driver.quit()  # closes all the browsers
