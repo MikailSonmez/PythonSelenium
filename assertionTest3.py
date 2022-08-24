b6cf51a324e7081ce3bda996f73b04693543a1ad
Mikail Sonmez <mikail4458@hotmail.com>
2022-08-24 18:20:12 +0300

    v11

--- assertionTest3.py
+++ assertionTest3.py
@@ -3,12 +3,14 @@
 import unittest
 from selenium import webdriver
 
+
 class Test(unittest.TestCase):
     def testName(self):
         driver = webdriver.Chrome()
         # driver = None
         # self.assertIsNone(driver)
         self.assertIsNotNone(driver)
+
 
 if __name__ == "__main__":
     unittest.main()
